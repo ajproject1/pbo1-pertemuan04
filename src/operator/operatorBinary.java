6a2b06452d2c069cb0c89629a6b93cda416e10cc
ajproject1 <ahmadjalalluddinlast@gmail.com>
2020-04-29 23:04:15 +0800

    operator Binary Aritmatika

--- src/operator/operatorBinary.java
+++ src/operator/operatorBinary.java
@@ -1,4 +1,16 @@
 package operator;
 
 public class operatorBinary {
+    String nama = "ahmad jalalluddin";
+    int jumlahSks = 24;
+    double indeksPrestasiKumulatif = 3.89;
+
+    public static void main(String[] args) {
+        System.out.println(9+3);
+        System.out.println(9-3);
+        System.out.println(9*3);
+        System.out.println(9/3);
+        System.out.println(9%3);
+        System.out.println(9%2);
+    }
 }
